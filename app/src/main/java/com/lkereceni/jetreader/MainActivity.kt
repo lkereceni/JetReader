e4a93988d4d7e104c4f060a4e6e33e07dc58734d
lkereceni <lovro.kereceni@gmail.com>
2022-06-22 11:03:56 +0200

    Dependency injection setup

--- app/src/main/java/com/lkereceni/jetreader/MainActivity.kt
+++ app/src/main/java/com/lkereceni/jetreader/MainActivity.kt
@@ -13,30 +13,19 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.google.firebase.firestore.FirebaseFirestore
 import com.lkereceni.jetreader.ui.theme.JetReaderTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetReaderTheme {
-                val db = FirebaseFirestore.getInstance()
-                val user: MutableMap<String, Any> = HashMap()
-                user["firstName"] = "Joe"
-                user["lastName"] = "James"
-
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    db.collection("users")
-                        .add(user)
-                        .addOnSuccessListener {
-                            Log.d("FB", "onCreate: ${it.id}")
-                        }
-                        .addOnFailureListener {
-                            Log.d("FB", "onCreate: $it")
-                        }
+
                 }
             }
         }
