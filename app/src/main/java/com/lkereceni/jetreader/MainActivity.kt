45512df8e20610ada2d08284bbcbb8257a5027b3
lkereceni <lovro.kereceni@gmail.com>
2022-06-23 23:25:55 +0200

    Creating the Splash Screen

--- app/src/main/java/com/lkereceni/jetreader/MainActivity.kt
+++ app/src/main/java/com/lkereceni/jetreader/MainActivity.kt
@@ -4,14 +4,20 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.google.firebase.firestore.FirebaseFirestore
+import com.lkereceni.jetreader.navigation.ReaderNavigation
 import com.lkereceni.jetreader.ui.theme.JetReaderTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -21,13 +27,23 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             JetReaderTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-
-                }
+                ReaderApp()
             }
+        }
+    }
+}
+
+@Composable
+fun ReaderApp() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            ReaderNavigation()
         }
     }
 }
