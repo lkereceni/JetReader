015695c84e56884ee20b66c5cf03324a28d1aea4
lkereceni <lovro.kereceni@gmail.com>
2022-06-24 16:03:51 +0200

    Setting up the User Form

--- app/src/main/java/com/lkereceni/jetreader/screens/login/ReaderLoginScreen.kt
+++ app/src/main/java/com/lkereceni/jetreader/screens/login/ReaderLoginScreen.kt
@@ -1,32 +1,27 @@
 package com.lkereceni.jetreader.screens.login
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.text.KeyboardActionScope
 import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.lkereceni.jetreader.components.EmailInput
+import com.lkereceni.jetreader.components.PasswordInput
 import com.lkereceni.jetreader.components.ReaderLogo
 
+@ExperimentalComposeUiApi
 @Composable
 fun ReaderLoginScreen(navController: NavController) {
     Surface(modifier = Modifier.fillMaxSize()) {
@@ -36,13 +31,24 @@ fun ReaderLoginScreen(navController: NavController) {
             verticalArrangement = Arrangement.Top
         ) {
             ReaderLogo()
+            UserForm(
+                loading = false,
+                isCreateAccount = false
+            ) {
+                email, password ->
+                Log.d("Form", "ReaderLoginScreen: $email $password")
+            }
         }
     }
 }
 
 @ExperimentalComposeUiApi
 @Composable
-fun UserForm() {
+fun UserForm(
+    loading: Boolean = false,
+    isCreateAccount: Boolean = false,
+    onDone: (String, String) -> Unit = { email, password -> }
+) {
     val email = rememberSaveable { mutableStateOf("") }
     val password = rememberSaveable { mutableStateOf("") }
     val passwordVisibility = rememberSaveable { mutableStateOf(false) }
@@ -62,9 +68,23 @@ fun UserForm() {
     ) {
         EmailInput(
             emailState = email,
-            enabled = true,
+            enabled = !loading,
             onAction = KeyboardActions {
                 passwordFocusRequest.requestFocus()
+            }
+        )
+        PasswordInput(
+            modifier = Modifier.focusRequester(passwordFocusRequest),
+            passwordState = password,
+            labelId = "Password",
+            enabled = !loading,
+            passwordVisibility = passwordVisibility,
+            onAction = KeyboardActions {
+                if (!valid) return@KeyboardActions
+                onDone(
+                    email.value.trim(),
+                    password.value.trim()
+                )
             }
         )
     }
