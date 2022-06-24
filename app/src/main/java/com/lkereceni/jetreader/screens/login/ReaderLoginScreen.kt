eff500420167127331d583477aecab336b2c2104
lkereceni <lovro.kereceni@gmail.com>
2022-06-24 12:31:42 +0200

    Setting up the Email InputField

--- app/src/main/java/com/lkereceni/jetreader/screens/login/ReaderLoginScreen.kt
+++ app/src/main/java/com/lkereceni/jetreader/screens/login/ReaderLoginScreen.kt
@@ -3,21 +3,28 @@ package com.lkereceni.jetreader.screens.login
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActionScope
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.ExperimentalComposeApi
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.lkereceni.jetreader.components.EmailInput
 import com.lkereceni.jetreader.components.ReaderLogo
 
 @Composable
@@ -53,6 +60,12 @@ fun UserForm() {
         modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
+        EmailInput(
+            emailState = email,
+            enabled = true,
+            onAction = KeyboardActions {
+                passwordFocusRequest.requestFocus()
+            }
+        )
     }
 }
